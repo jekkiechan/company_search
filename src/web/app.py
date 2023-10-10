c073a382aba5251ad8d57ff1175af3b3f154e176
ratanaruengj <trata@sertiscorp.com>
2023-10-10 23:26:25 +0900

    refactor: streamlit app to make it looks nicer

--- src/web/app.py
+++ src/web/app.py
@@ -1,21 +1,41 @@
 import streamlit as st
+import pandas as pd
 
 from src.preprocess.preprocess import preprocess
 from src.agent.dataframe_agent import DataFrameAgent
 
-
 DATA_DIR = "data/techsauce_companies.csv"
-data = preprocess(DATA_DIR)
+data = pd.read_csv(DATA_DIR)
+data = preprocess(data)
 agent = DataFrameAgent(data=data)
 
 
 def main():
     st.title("Company-Startup Interaction Query System")
 
-    st.sidebar.header("Query Input")
-    st.sidebar.write("Enter your question below:")
-    user_input = st.sidebar.text_input(
-        "", "What startups did company A meet with on August 16?"
+    st.markdown("""
+        <style>
+            .reportview-container {
+                background-color: #f0f0f5;
+                color: #111;
+            }
+            .big-font {
+                font-size:20px !important;
+            }
+            .text-input {
+                width:70%;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.header("Query Input")
+    st.write("Enter your question below:", className="big-font")
+
+    user_input = st.text_input(
+        label="This is a text",
+        value=None,
+        label_visibility="hidden",
+        placeholder="What startups did company A meet with on August 16?",
     )
 
     if user_input:
