13966df1cce66edc0ccb334f1a8f37589a2defb6
ratanaruengj <trata@sertiscorp.com>
2023-10-11 10:49:48 +0900

    refactor: app.py to root

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-import sys
-import os
-
 import streamlit as st
 import pandas as pd
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(os.path.dirname(current_dir))
-sys.path.append(parent_dir)
 from src.preprocess.preprocess import preprocess
 from src.agent.dataframe_agent import DataFrameAgent
 
